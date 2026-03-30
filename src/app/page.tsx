2a245bd5b1a7a4c732332ae7d33c63603147c37a
u6lqre <ozwtn9@tutamail.com>
2026-03-30 16:43:53 +0200

    add footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+import Footer from "@/components/footer";
 import Hero from "@/components/hero";
-import styles from "./page.module.css";
 import Plans from "@/components/plans";
+import styles from "./page.module.css";
 
 function Home() {
   return (
     <main className={styles.main}>
       <Hero />
       <Plans />
+      <Footer />
     </main>
   );
 }
